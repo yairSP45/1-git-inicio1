5c57ea07e61c6543564e47f0b0d73483d117bba8
yairSP45 <sanabria.porras.yair@gmail.com>
2023-08-21 11:07:10 -0600

    terminamos la practica

---
+++ js/index.js
@@ -0,0 +1,31 @@
+console.log('@@@ conectado')
+
+const titulo = document.getElementsByClassName('titulo')[0]
+titulo.style.color= 'red'
+
+titulo.addEventListener('hover', ()=> {
+    titulo.style.color= 'black'
+})
+console.log('@@@ titulo', titulo)
+
+const num1 = document.getElementById('num1')
+const num2 = document.getElementById('num2')
+const btnSuma = document.getElementById('btnSuma')
+
+btnSuma.addEventListener('click', () => {
+    sumaValores()
+})
+
+const sumaValores = () => {
+    console.log('@@@ Valores', parseInt(num1.value), parseInt(num2.value))
+    if(!isNaN(parseInt(num1.value)) && !isNaN(parseInt(num2.value))){
+        const res = parseInt(num1.value) + parseInt(num2.value)
+        alert('El resultado es: ', + res)
+    }
+}
+
+/*
+function sumaValores() {
+
+}
+*/
